aebe1489ada5c336505a68b80008c8abcb6c2137
mouzong <andreasmouzong@gmail.com>
2022-03-23 09:04:12 +0100

    Initial commit with the project bootstrapping

---
+++ README.md
@@ -0,0 +1,10 @@
+# Springboot Restful API for employee management
+
+##Tech Stack
+
+- Springboot
+- Springframework
+- Postgresql
+- Hybernate
+- SpringData JPA
+- Swagger UI
---
+++ src/test/java/com/codegrill/employee/EmployeeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegrill.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
